c519164a50102d3a45190cf72b13d0682261d91d
Mike <msoltiz@gmail.com>
2024-10-23 01:46:24 +0000

    added necessary files and modifications to get app up and running on replit.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from app import create_app
 from flask_socketio import SocketIO
 from app.game import register_socketio_events
+import eventlet
+
+eventlet.monkey_patch()
 
 app = create_app()
 socketio = SocketIO(app)
 register_socketio_events(socketio)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080)
